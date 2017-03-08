865c8cf59770fbf130aff06eb7aa0629dcbceb75
Maddie <mk186442@falmouth.ac.uk>
2017-03-08 11:38:34 +0000

    Created Java project for client

---
+++ src/Game.java
@@ -0,0 +1,30 @@
+
+class Game 
+{
+       
+    /**
+     * Main method to start the game.
+     */
+    public static void main(String[] args)
+    {
+    	new Game().play();
+    }
+    
+    /**
+     * Create the game and initialise its internal map.
+     */
+    public Game() 
+    {
+        
+    }
+
+    /**
+     *  Main play routine.  Loops until end of play.
+     */
+    public void play() 
+    {            
+        
+    }
+        
+
+}
