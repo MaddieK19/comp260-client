ec66ed445897fe93fd788f88243f8d6d4a55ff68
Maddie <mk186442@falmouth.ac.uk>
2017-03-13 13:45:30 +0000

    Added TowerManager.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,15 +5,14 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    public float speed;             //Floating point variable to store the player's movement speed.
+    public float speed;
 
-    private Rigidbody2D rb2d;       //Store a reference to the Rigidbody2D component required to use 2D Physics.
-    private int count;              //Integer to store the number of pickups collected so far.
+    private Rigidbody2D rb2d;
+    private int count;
 
     // Use this for initialization
     void Start()
     {
-        //Get and store a reference to the Rigidbody2D component so that we can access it.
         rb2d = GetComponent<Rigidbody2D>();
 
     }
---
+++ Assets/Scripts/TowerManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TowerManager : MonoBehaviour {
+    public List<GameObject> towers;
+    public GameObject towerPrefab;
+    int maxTowerNumber = 10;
+    public string key = "a";
+
+    private PlayerController player;
+
+    // Use this for initialization
+    void Start () {
+        player = GetComponent<PlayerController>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void addTower(Vector3 towerPosition)
+    {
+        GameObject newTower = Instantiate(towerPrefab, towerPosition, Quaternion.identity) as GameObject;
+        towers.Add(newTower);
+    }
+
+    void destroyTower()
+    {
+
+    }
+
+    void checkForInput()
+    {
+        if (Input.GetKeyDown(key))
+        {
+            addTower(player.transform.position);
+        }
+    }
+}
