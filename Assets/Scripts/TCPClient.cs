f885fdd9320e1f9e0f8ed395d3d4bd660b4f0829
Maddie <mk186442@falmouth.ac.uk>
2017-03-13 14:10:30 +0000

    Added TCPClient

---
+++ Assets/Scripts/TCPClient.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class TCPClient : MonoBehaviour {
+    private int socketNumber;
+    private string host = "localhost";
+    TCPClient client;
+
+    // Use this for initialization
+    void Start () {
+        client = new TCPClient();
+        client.socketNumber = socketNumber;
+        client.host = host;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+
+    }
+}
