75f7da83120a10510125f1e56e3b5439ef4d8ad8
Maddie <mk186442@falmouth.ac.uk>
2017-04-16 14:01:49 +0100

    Added a timer to control server updates

--- Client/Game.cs
+++ Client/Game.cs
@@ -3,17 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
 
 namespace Client
 {
     class Game
     {
         TCPClient client;
-        
+        Timer timer;
+
+        float serverUpdateInterval = 200;
+ 
+// Implement a call with the right signature for events going off
+private void myEvent(object source, ElapsedEventArgs e) { }
+
         public Game()
         {
             client = new TCPClient();
             client.initServerConnection();
+            initTimer();
+        }
+
+        void initTimer()
+        {
+            timer = new Timer();
+            timer.Elapsed += new ElapsedEventHandler(myEvent);
+            timer.Interval = serverUpdateInterval;
+            timer.Enabled = true;
         }
 
         public void run()
